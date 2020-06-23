b610836de060b3a14d76c366a7a19cabb83ca115
shahabkhan22 <shahab2205@gmail.com>
2020-06-24 00:17:08 +0530

    JSON Parsing

--- DataToJSON/marshal.go
+++ DataToJSON/marshal.go
@@ -12,35 +12,39 @@ typed language and JSON is a dynamically typed language.
 
 */
 type People struct {
-	fname string
-	lname string
-	age   int
+	Fname string
+	Lname string
+	Age   int
 }
 
 func main() {
 	p1 := People{
-		fname: "ABC",
-		lname: "XYZ",
-		age:   27,
+		Fname: "ABC",
+		Lname: "XYZ",
+		Age:   27,
 	}
 
 	p2 := People{
-		fname: "PQR",
-		lname: "XYZ",
-		age:   26,
+		Fname: "PQR",
+		Lname: "XYZ",
+		Age:   26,
 	}
 
 	PAll := []People{
 		p1,
 		p2,
 	}
-
+	fmt.Println("This is a struct and its type")
+	fmt.Printf("%T\n", PAll)
 	fmt.Println(PAll)
 
 	bs, err := json.Marshal(PAll)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	fmt.Println("This is a JSON of our above Struct and its type")
+	fmt.Printf("%T\n", bs) // this will give us uint8
 	fmt.Println(string(bs))
 	fmt.Println(bs)
 
